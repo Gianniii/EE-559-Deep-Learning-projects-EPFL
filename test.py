98ad7c238c8f92181d7d401cb816a7559efe1d64
Luca Bracone <lucabr123@gmail.com>
2021-04-28 16:03:21 +0200

    first terrible implementation

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 import torch
+from torchvision import datasets
+import os
 from torch import nn
 from torch.nn import functional as F
 
 def generate_pair_sets(nb):
-    if args.data_dir is not None:
-        data_dir = args.data_dir
-    else:
-        data_dir = os.environ.get('PYTORCH_DATA_DIR')
-        if data_dir is None:
-            data_dir = './data'
-
+    data_dir = os.environ.get('PYTORCH_DATA_DIR')
+    if data_dir is None:
+        data_dir = './data'
     train_set = datasets.MNIST(data_dir + '/mnist/', train = True, download = True)
     train_input = train_set.data.view(-1, 1, 28, 28).float()
     train_target = train_set.targets
@@ -23,27 +21,29 @@ def generate_pair_sets(nb):
 
 class NeuralNet(nn.Module):
     def __init__(self):
+        super().__init__()
         self.fc1 = nn.Linear(392, 200)
-        self.fc2 = nn.Linear(200, 100)
-        self.fc3 = nn.Linear(100, 2)
+        self.fc2 = nn.Linear(200, 50)
+        self.fc3 = nn.Linear(50, 2)
 
     def forward(self, x):
         x = F.relu(self.fc1(x.view(-1, 392)))
         x = F.relu(self.fc2(x.view(-1, 200)))
-        x = self.fc3(x.view(-1, 100))
+        x = self.fc3(x.view(-1, 50))
         return x
 
-criterion = nn.MSELoss()
+criterion = nn.CrossEntropyLoss()
 eta, mini_batch_size = 1e-1, 100
 nb_epochs = 25
 
 def compute_nb_errors(model, input, target, mini_batch_size):
     count = 0
-    output = model(input)
-    for b in range(output.size(0)):
-        _, j = output[b,:].max(0)
-        if target[b,j] < 0.5:
-            count += 1
+    for b in range(0,train_input.size(0), mini_batch_size):
+        output = model(input.narrow(0, b, mini_batch_size))
+        _, predicted_classes = output.max(1)
+        for k in range(mini_batch_size):
+            if target[b + k] != predicted_classes[k]:
+                count = count + 1
     return count
 
 def train_model(model, train_input, train_target, mini_batch_size):
@@ -72,3 +72,11 @@ def mnist_to_pairs(nb, input, target):
     classes = target[a]
     target = (classes[:, 0] <= classes[:, 1]).long()
     return input, target, classes
+
+train_input, train_target, train_classes, test_input, \
+test_target, test_classes = generate_pair_sets(1000)
+
+model = NeuralNet()
+train_model(model, train_input, train_target, mini_batch_size)
+n = compute_nb_errors(model, test_input, test_target, mini_batch_size )
+print(n)
