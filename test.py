9a6c981ff8fd9f2e6c264a79c3c3a8505ea8ac49
Luca Bracone <lucabr123@gmail.com>
2021-04-28 14:19:01 +0200

    second commit !!

---
+++ test.py
@@ -0,0 +1,18 @@
+def generate_pair_sets(nb):
+    if args.data_dir is not None:
+        data_dir = args.data_dir
+    else:
+        data_dir = os.environ.get('PYTORCH_DATA_DIR')
+        if data_dir is None:
+            data_dir = './data'
+
+    train_set = datasets.MNIST(data_dir + '/mnist/', train = True, download = True)
+    train_input = train_set.data.view(-1, 1, 28, 28).float()
+    train_target = train_set.targets
+
+    test_set = datasets.MNIST(data_dir + '/mnist/', train = False, download = True)
+    test_input = test_set.data.view(-1, 1, 28, 28).float()
+    test_target = test_set.targets
+
+    return mnist_to_pairs(nb, train_input, train_target) + \
+           mnist_to_pairs(nb, test_input, test_target)
