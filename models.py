7dc12c6ae3f17d83a3bbdea69e412752a92f6974
Luca Bracone <lucabr123@gmail.com>
2021-05-01 16:16:46 +0200

    cnn

--- models.py
+++ models.py
@@ -11,26 +11,32 @@ class BasicNN(nn.Module):
         self.bn2 = nn.BatchNorm1d(100)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x.view(-1, 392)))
-        x = self.bn1(x)
-        x = F.relu(self.fc2(x.view(-1, 100)))
-        x = self.bn2(x)
+        x = F.relu(self.bn1(self.fc1(x.view(-1, 392))))
+        x = F.relu(self.bn2(self.fc2(x.view(-1, 100))))
         x = self.fc3(x.view(-1, 100))
         return x
 
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.c1 = nn.Conv2d(2, 32, kernel_size = 5)
-        self.c2 = nn.Conv2d(32, 64, kernel_size = 5)
-        self.fc3 = nn.Linear(100, 2)
-        self.bn1 = nn.BatchNorm1d(100)
-        self.bn2 = nn.BatchNorm1d(100)
+        self.conv1 = nn.Conv2d(2, 32, kernel_size = 5)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size = 3)
+        self.fc1 = nn.Linear(256, 100)
+        self.fc2 = nn.Linear(100, 2)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x.view(-1, 392)))
+        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size = 2, stride = 2))
+        #print("a", x.size())
         x = self.bn1(x)
-        x = F.relu(self.fc2(x.view(-1, 100)))
+        #print("b", x.size())
+        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size = 2, stride = 1))
+        #print("c", x.size())
         x = self.bn2(x)
-        x = self.fc3(x.view(-1, 100))
+        #print("d", x.size())
+        x = F.relu(self.fc1(x.view(100, -1)))
+        #print("d2", x.size())
+        x = F.relu(self.fc2(x))
+        #print("e", x.size())
         return x
