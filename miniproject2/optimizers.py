b5d3022f14d695cfdf36476acf3b41ea00d5ffe8
Gianniii <gianni.lodetti@hotmail.com>
2021-05-23 19:14:25 +0200

    added SGD

---
+++ miniproject2/optimizers.py
@@ -0,0 +1,17 @@
+
+
+class SGD():
+    '''
+    Stochastic gradient descent.
+    '''
+    def init(self, model, lr=0.001):
+        if lr < 0.0:
+            raise ValueError("negative learning rate".format(lr))
+        self.params = model.param()
+        self.lr = lr
+
+    def step(self):
+        for param in self.params:
+            weight, grad = param
+            #update weights
+            weight.add_(-self.lr * grad)
