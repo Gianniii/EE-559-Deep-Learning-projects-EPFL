5ec823b81f78ff0301f3456f909d42dbf3533ae2
Gianniii <gianni.lodetti@hotmail.com>
2021-05-25 22:19:37 +0200

    added different initializations for Relu

--- miniproject2/modules.py
+++ miniproject2/modules.py
@@ -26,34 +26,34 @@ class Module(object):
 
 # Module for fully-connected layer
 class Linear(Module):
-    def __init__(self, input_layer_size, output_layer_size):
+    def __init__(self, input_layer_size, output_layer_size, paramInit = "Normal"):
         super().__init__()
         # Weights "w" is a 2d tensor [input_layer_size, output_layer_size]
         # which is the transpose of what might seem "logical"
         # Thanks to broadcasting "w" is going to increase to a 3d tensor when we receive a batch of inputs
         # Bias "b" is a 1d tensor [output_layer_size]
         # We initialize with Xavier method
-        variance = 2.0 / (input_layer_size + output_layer_size)
-        self.w = torch.empty(input_layer_size, output_layer_size).normal_(0, 1)
-        self.b = torch.empty(output_layer_size).normal_(0, 1)
-
+        #variance = 2.0 / (input_layer_size + output_layer_size)
+        #self.w = torch.empty(input_layer_size, output_layer_size).normal_(0, 1)
+    
+        var = 1 #normal by default
+        self.w = torch.empty(input_layer_size, output_layer_size).normal_(0, var) #normal by default
+        if paramInit == "He": 
+            # 'He initialization' recommends for layers with a ReLU activation
+            var = math.sqrt(2/(input_layer_size))
+        if paramInit == "Xavier":
+            # 'Xavier initialization' recommends for layers with a tanh activation
+            var =  math.sqrt(2/(input_layer_size + output_layer_size))
+            
+        self.w = torch.empty(input_layer_size, output_layer_size).normal_(0, var)
+        
+        self.b = torch.empty(output_layer_size).normal_(0, var)
         # Gradient vector is just empty for now
         # Each channel represents one of the inputs we receive in the batch
         # And within each channel, each entry represents "how much" the weight should change according to that x
         self.grad_w = torch.empty(self.w.size()).fill_(0)
         self.grad_b = torch.empty(self.b.size()).fill_(0)
 
-        #self.w = FloatTensor(input_layer_size, output_layer_size)
-        #self.grad_w = FloatTensor(input_layer_size, output_layer_size)
-        
-        #self.b = FloatTensor(output_layer_size)
-        #self.grad_b = FloatTensor(output_layer_size)
-
-        #init parameters with normal distribution
-        #self.w.normal_() 
-        #self.grad_w.fill_(0)
-        #self.b.normal_()
-        #self.grad_b.fill_(0)
 
     def forward(self, x):
         # We record the input for later use
@@ -134,7 +134,6 @@ class Tanh(Module):
     def backward(self, dl_dout):
         return 4.0 * (self.x.exp() + (-self.x).exp()).pow(-2) * dl_dout
 
-#UNTESTED
 class Sigmoid(Module):
     def forward(self, x):
         self.x = x.clone()
@@ -159,4 +158,18 @@ class MSELoss(Module):
 
     def backward(self):
         return torch.div(self.input - self.target, self.input.size(0)) * 2
+
+#class CrossEntropyLoss(Module):
+    #def __init__(self) -> None:
+    #    super().__init__()
+
+    #def forward(self, input, target):
+    #    sig = input
+    #    self.p = sig
+    #    self.y = target.view(input.shape)
+    #    loss = self.y*(self.p.log()) - (1-self.y) * (1 -self.p)
+    #    return loss
+
+    #def backward(self):
+    #    return ((-self.y)/self.p) + (1-self.y)/(1-self.p)
     
